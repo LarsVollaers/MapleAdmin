82fccf4c818804b4731b5bb97dc248afc6d328ae
holthelper <holt.helper@gmail.com>
2016-07-07 15:31:44 -0700

    v1.6
    -made somewhat usable as standalone
    -fixed character update (int) and got rid of GD Files will add back
    later with zip file.

--- MapleAdmin/src/header.php
+++ MapleAdmin/src/header.php
@@ -1,29 +1,25 @@
 <?php
-if(basename($_SERVER["PHP_SELF"]) == "header.php"){
+if(basename($_SERVER["PHP_SELF"]) == "header.php") {
     die("403 - Access Forbidden");
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>MapleAdmin</title>
-	<link rel="icon" href="favicon.ico" type="image/x-icon" />
-	<link rel="stylesheet" href="config/css/bootstrap.css">
-	<link rel="stylesheet" href="config/css/style.css">
-	<link rel="stylesheet" href="config/css/bootstrap.min.css">
-	<script type="text/javascript" src="config/js/jquery.min.js"></script>
-	<script type="text/javascript" src="config/js/bootstrap.min.js"></script>
-	
-</head>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>MapleAdmin</title>
+		<link rel="stylesheet" href="./config/css/bootstrap.css">
+		<link rel="stylesheet" href="./config/css/style.css">
+		<link rel="stylesheet" href="./config/css/bootstrap.min.css">
+		<script type="text/javascript" src="./config/js/jquery.min.js"></script>
+		<script type="text/javascript" src="./config/js/bootstrap.min.js"></script>
+	</head>
 	<body>
 		<div class="col-xs-12-fluid">
 			<div class="well well-sm">
 					<div class="logo_box">
-						<div class="version_fix">
-							<b><?php echo''.$siteversion.'';?></b>
-						</div>
+						<div class="version_fix"><b><?php echo $siteversion; ?></b></div>
 					</div> 
 			</div>
 		</div>
